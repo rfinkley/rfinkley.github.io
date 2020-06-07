a34b78963937857066a56a5181c740ef49d1d0be
Robert Finkley <rfinkley@gmail.com>
2020-06-06 20:25:00 -0400

    Added scroll to top button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,26 @@
-const toggleDisplay = function (id) {
-    if ($(`#${id}`).attr("data-visible")==="false") {
-        $("section[data-visible='true']").removeClass('d-block');
-        $("section[data-visible='true']").addClass('d-none');
-        $("section[data-visible='true']").attr('data-visible', 'false');
-        $(`#${id}`).attr("data-visible", "true");
-        $(`#${id}`).removeClass("d-none");
-        $(`#${id}`).addClass("d-block");
-    }
-};
+// const toggleDisplay = function (id) {
+//     if ($(`#${id}`).attr("data-visible")==="false") {
+//         $("section[data-visible='true']").removeClass('d-block');
+//         $("section[data-visible='true']").addClass('d-none');
+//         $("section[data-visible='true']").attr('data-visible', 'false');
+//         $(`#${id}`).attr("data-visible", "true");
+//         $(`#${id}`).removeClass("d-none");
+//         $(`#${id}`).addClass("d-block");
+//     }
+// };
 
+  const scrollTop = document.querySelector('#scrollTop');
+  
+  function checkPosition() {
+    let windowY = window.scrollY;
+    if (windowY > 250) {
+      scrollTop.classList.add('is-visible');
+      scrollTop.classList.remove('is-hidden');
+    } else {
+      // Scrolling DOWN
+      scrollTop.classList.add('is-hidden');
+      scrollTop.classList.remove('is-visible');
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
